97e472e68df3aeae57dbce85b0f008808d3c3969
DESKTOP-5UQBPTV\colorverse <seokhwa.lee@colorverse.co>
2023-01-05 15:12:59 +0900

    ADD: 테스트 페이지 추가

---
+++ pages/test/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Page_Test: NextPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Test Page</h1>
+    </div>
+  );
+};
+
+export default Page_Test;
